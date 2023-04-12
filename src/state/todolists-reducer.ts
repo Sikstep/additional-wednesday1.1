11c795391255db0eaa1c8345523c9b5ec78e4f19
Sikstep <Sikstep1988@gmail.com>
2023-04-12 14:33:49 +0300

    first commit

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,31 @@
+import {v1} from 'uuid';
+import {FilterValuesType, TodolistType} from "../App";
+
+
+export type AddTodolistActionType = ReturnType<typeof addTodolistAC>
+
+
+
+
+type ActionsType = AddTodolistActionType
+
+const initialState: Array<TodolistType> =  []
+
+export const todolistsReducer = (state: Array<TodolistType> = initialState, action: ActionsType): Array<TodolistType> => {
+debugger
+    switch (action.type) {
+        case 'АDD-TODOLIST': {
+            return [{
+                id: action.todolistId,
+                title: action.title,
+                filter: 'all'
+            }, ...state]
+        }
+        default:
+            return state;
+    }
+}
+
+
+export const addTodolistAC = (title: string) => ({ type: 'АDD-TODOLIST', title: title, todolistId: v1()}  as const)
+
